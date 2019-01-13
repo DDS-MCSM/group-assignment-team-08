e10fcc86ca702973404b04137bdab21d1749730d
Rodrigo Vera <rodriss.axs@gmail.com>
2019-01-13 03:53:22 +0100

    Se mejoro los df y se agrego mas graficos

--- R/code.R
+++ R/code.R
@@ -5,16 +5,6 @@
 #' @export
 #'
 #' @examples
-a <- read.csv(url("https://ransomwaretracker.abuse.ch/feeds/csv/"), skip = 8, header = TRUE, stringsAsFactors = F, check.names = T)
-mynames <- colnames(a)
-mynames[1] <- "Firstseen_UTC"
-colnames(a) <- mynames
-mynames <- c("Firstseen_UTC","Threat","Malware","Status","Country")
-data <- a[mynames]
-data$Firstseen_UTC <- format(as.Date(data$Firstseen_UTC), "%Y-%m-%d")
-
-CiudadesRaras <- data[grep(pattern = "\\|", x = data$Country, perl = F), ]
-dataFinal <- data[!grepl(pattern = "\\|", x = data$Country, perl = F), ]
 
 
 if (!require(rworldmap)) install.packages('rworldmap')
@@ -22,36 +12,21 @@ if (!require(ggmap)) install.packages('ggmap')
 library(ggmap)
 library(rworldmap)
 
-
-
-sPDF <- joinCountryData2Map(dF = dataFinal,
-                            joinCode = "ISO2",
-                            nameJoinColumn = "Country",
-                            verbose = T)
-
-# plot the spatialDataFrame
-
-mapCountryData(sPDF, nameColumnToPlot = "Malware")
-mapCountryData(sPDF, nameColumnToPlot = "Threat")
-mapCountryData(sPDF, nameColumnToPlot = "Status")
-
-
-# 
-# f <- data$Firstseen_UTC
-# f <- as.Date(f, %Y)
-# c <- data$Country
-# 
-# > fechas <- strptime(x = head(data$Firstseen_UTC), format = "%Y-%m-%d")
-# > months.Date(fechas)
-# data$Firstseen_UTC <- format(as.Date(data$Firstseen_UTC), %Y %m %d)
-# > months.Date(fechas)
-# [1] "agosto" "agosto" "agosto" "agosto" "agosto" "agosto"
-# > f <- data$Country
-# > kk <- grep(pattern = ".*|.*", x = f)
-# > kk <- grep(pattern = ".*|.*", x = f, perl = F)
-# > kk <- grep(pattern = "|", x = f, perl = F)
-# > kk <- grep(pattern = "\|", x = f, perl = F)
-# Error: '\|' is an unrecognized escape in character string starting ""\|"
-# > kk <- grep(pattern = "\\|", x = f, perl = F)
-# > raros <- data[grep(pattern = "\\|", x = f, perl = F), ]
-# > no.raros <- data[!grepl(pattern = "\\|", x = f, perl = F), ]
+obtenerDf <- function(){
+  a <- read.csv(url("https://ransomwaretracker.abuse.ch/feeds/csv/"), skip = 8, header = TRUE, stringsAsFactors = F, check.names = T)
+  mynames <- colnames(a)
+  mynames[1] <- "Firstseen_UTC"
+  colnames(a) <- mynames
+  mynames <- c("Firstseen_UTC","Threat","Malware","Status","Country")
+  data <- a[mynames]
+  data$Firstseen_UTC <- format(as.Date(data$Firstseen_UTC), "%Y-%m-%d")
+  data$Firstseen_UTC <- as.Date(data$Firstseen_UTC)
+  
+  CiudadesRaras <- data[grep(pattern = "\\|", x = data$Country, perl = F), ]
+  dataFinal <- data[!grepl(pattern = "\\|", x = data$Country, perl = F), ]
+  
+  dataFinal <- dataFinal[!apply(dataFinal, 1, function(x) any(x=="")),]
+  dataFinal <- dataFinal[!is.na(dataFinal$Firstseen_UTC),]
+  
+  return(dataFinal)
+}
